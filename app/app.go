b981961729ee1a2216308011de743117ed005cbf
otomarukanta <kanta208@gmail.com>
2018-04-22 10:20:28 +0900

    Add cors settings

--- app/app.go
+++ app/app.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
 	"github.com/otomarukanta/nokia-api/handler"
 )
 
@@ -15,6 +16,16 @@ func init() {
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	})
+	r.Use(c.Handler)
 
 	r.Get("/measure", handler.GetMeasure)
 
